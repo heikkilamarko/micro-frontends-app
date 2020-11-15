e315936a76f1ef66d915dd4c9a3a462211f1bbd3
Marko Heikkilä <heikkila_marko@hotmail.com>
2020-11-15 12:49:24 +0200

    Snowpack migration.

---
+++ app-shell/snowpack.config.js
@@ -0,0 +1,30 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_",
+  },
+  plugins: [
+    "@snowpack/plugin-svelte",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-optimize",
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
